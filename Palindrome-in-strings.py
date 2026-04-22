60bb826e727f139f9d9e74e76ec1dcd45aec6d5c
Arsh Khandelwal <khandelwalarsh2774@gmail.com>
2026-04-22 22:45:50 +0530

    Implement multiple palindrome checking methods
    Added two additional methods to check for palindromes in strings.

--- Palindrome-in-strings.py
+++ Palindrome-in-strings.py
@@ -6,3 +6,37 @@ else:
   print("It's not a palindrome")
 
 #Type - II
+a = input("Enter the word to be checked: ")
+c = []
+for i in range(len(a)):
+    c.append(a[len(a) - 1 - i])
+count = 0
+for i in range(0,len(a)):
+    if c[i] == a[i]:
+        count += 1
+if count == len(a):
+    print("It's a palindrome")
+else:
+    print("It isn't a palindrome")
+
+#Type - III
+a = input("Enter the word to be checked: ")
+c = []
+if len(a)%2 == 0:
+    count = 0
+    for i in range(0,int(len(a)/2)):
+        if a[i] == a[len(a) - i -1]:
+            count += 1
+    if count == i+1:
+        print(f"{a} is a palindrome")
+    else:
+        print(f"{a} isn't a palindrome")
+else:
+    count = 0
+    for i in range(0,int((len(a)-1)/2)):
+        if a[i] == a[len(a) - i -1]:
+            count += 1
+    if count == i+1:
+        print(f"{a} is a palindrome")
+    else:
+        print(f"{a} isn't a palindrome")
