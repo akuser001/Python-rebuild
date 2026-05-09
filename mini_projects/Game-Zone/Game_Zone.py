fe7e1bd0bede9c59bd4686447e2c8589abbf576c
Arsh Khandelwal <khandelwalarsh2774@gmail.com>
2026-05-09 22:02:05 +0530

    Add Tic Tac Toe module to main file

--- mini_projects/Game-Zone/Game_Zone.py
+++ mini_projects/Game-Zone/Game_Zone.py
@@ -1,6 +1,7 @@
 #Game-Zone, a collection of mini-games for entertainment.
 import Hangman
 import Number_guess
+import Tic_Tac_Toe as Tic
 def main_menu():
     print("Welcome to Game-Zone!")
     print("1. Play Hangman")
@@ -9,7 +10,9 @@ def main_menu():
     print("4. Exit")
     choice = input("Enter your choice: ")
     return choice
-def hangman():
+def hang():
     Hangman.main()
-def number_guess():
+def num_g():
     Number_guess.main()
+def ttt():
+    Tic.main()
